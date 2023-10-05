bda221750f3d1321cf19a12f423b76d8e58a66ec
Brandon <brandontang0325@gmail.com>
2023-10-05 09:00:36 +0100

    add s-r article

---
+++ src/routes/article/spaced-repetition/+page.svelte
@@ -0,0 +1,97 @@
+<div class="page-content">
+  <section>
+    <article>
+      <h2>What is Spaced Repetition?</h2>
+      <h5>
+        Learn how to become a better player by utilising spaced
+        repetition to help you memorise and recognise patterns more
+        efficiently.
+      </h5>
+      <img src="/images/SRgraph.png" alt="spaced repetition graph" />
+
+      <h3>Lorem ipsum dolor sit amet consec</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et
+        massa mi. Aliquam in hendrerit urna. Pellentesque sit amet
+        sapien fringilla, mattis ligula consectetur, ultrices mauris.
+        Maecenas vitae mattis tellus. Nullam quis imperdiet augue.
+        Vestibulum auctor ornare leo, non suscipit magna interdum eu.
+        Curabitur pellentesque nibh nibh, at maximus ante fermentum
+        sit amet. Pellentesque commodo lacus at sodales sodales.
+        Quisque sagittis orci ut diam condimentum, vel euismod erat
+        placerat. In iaculis arcu eros, eget tempus orci facilisis id.
+        Praesent lorem orci, mattis non efficitur id, ultricies vel
+        nibh. Sed volutpat lacus vitae gravida viverra. Fusce vel
+        tempor elit. Proin tempus, magna id scelerisque vestibulum,
+        nulla ex pharetra sapien, tempor posuere massa neque nec
+        felis. Aliquam sem ipsum, vehicula ac tortor vel, egestas
+        ullamcorper dui. Curabitur at risus sodales, tristique est id,
+        euismod justo. Mauris nec leo non libero sodales lobortis.
+        Quisque a neque pretium, dictum tellus vitae, euismod neque.
+        Nulla facilisi. Phasellus ultricies dignissim nibh ut cursus.
+        Nam et quam sit amet turpis finibus maximus tempor eget augue.
+        Aenean at ultricies lorem. Sed egestas ligula tortor, sit amet
+        mattis ex feugiat non. Duis purus diam, dictum et ante at,
+        commodo iaculis urna. Aenean lacinia, nisl id vehicula
+        condimentum, enim massa.
+      </p>
+      <h3>Ullam aliquam</h3>
+      <p>
+        Consectetur adipisicing elit. Provident accusamus ipsam
+        temporibus quibusdam, ipsa dolorem dolorum odio eligendi optio
+        inventore perferendis porro officia, at natus, itaque quo et
+        hic repellendus nesciunt eos tenetur quasi ex quia nam!
+        Blanditiis, culpa? Beatae voluptatibus quidem quasi et
+        deserunt enim ad similique iusto repellendus placeat incidunt
+        quo, id, culpa odio, a qui pariatur veniam consequuntur
+        cupiditate fugit quae non? Aspernatur corrupti eveniet nihil
+        non quibusdam, sed harum quasi officiis ullam perferendis
+        voluptatum reprehenderit esse cupiditate soluta deleniti nisi
+        ipsum, illum architecto labore ad sint. Sunt quidem a harum
+        magni cupiditate veniam velit itaque ipsa maxime consequuntur
+        iure autem numquam obcaecati qui, dolorem vero eveniet
+        expedita! Nostrum deserunt facere consectetur recusandae
+        architecto sit fugiat nisi placeat assumenda debitis
+        necessitatibus, suscipit omnis dolorum quibusdam fuga quis
+        odio cumque repellendus dignissimos. Ipsa pariatur placeat
+        adipisci, doloremque autem, ex voluptates voluptatum
+        consequatur corrupti sit, minus ullam maxime labore molestias
+        delectus debitis omnis dolore alias vero rem enim dolorem quis
+        dicta voluptate. Inventore quibusdam, molestiae illum neque
+        aspernatur odit id dignissimos sunt distinctio voluptatibus
+        similique officia quae non architecto eveniet voluptatum sint!
+        Tempora, minima officia? Vero illum tenetur pariatur sunt
+        perferendis doloribus quisquam temporibus distinctio error
+        fugit, quas culpa?
+      </p>
+    </article>
+  </section>
+</div>
+
+<style>
+  article {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    margin: 5rem 0;
+  }
+  h2,
+  h5 {
+    text-align: center;
+  }
+  h2 {
+    margin-bottom: 1rem;
+  }
+  img {
+    width: min(100%, 45rem);
+    align-self: center;
+    margin-bottom: 5rem;
+  }
+  h5,
+  p {
+    line-height: 150%;
+  }
+  p {
+    font-size: 1.25rem;
+  }
+</style>
