473c1727e9dfca4506c1daf310f3a945883c9eb1
Brandon <brandontang0325@gmail.com>
2023-10-06 09:43:28 +0100

    Create Popup.svelte

---
+++ src/components/Popup.svelte
@@ -0,0 +1,39 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  import Svg from "./Svg.svelte";
+
+  const dispatch = createEventDispatcher();
+  function handleClose() {
+    dispatch("close");
+  }
+</script>
+
+<div class="popup-window popup">
+  <button class="close" on:click={handleClose}
+    ><Svg name="close" color="var(--secondary)" />
+  </button>
+  <slot />
+</div>
+
+<style>
+  .popup-window {
+    padding: 12%;
+    padding-bottom: 3.5rem;
+    border-radius: var(--br);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1.5rem;
+    position: relative;
+  }
+
+  button.close {
+    position: absolute;
+    top: 0.6rem;
+    right: 0.6rem;
+    background-color: var(--text);
+    width: 1.5rem;
+    height: 1.5rem;
+    border-radius: var(--br-small);
+  }
+</style>
