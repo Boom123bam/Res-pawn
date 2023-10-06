dce7eae553e6148ed24446cf4d90a073617b4a0a
Brandon <brandontang0325@gmail.com>
2023-10-06 09:11:14 +0100

    Update +page.svelte

--- src/routes/article/spaced-repetition/+page.svelte
+++ src/routes/article/spaced-repetition/+page.svelte
@@ -1,69 +1,99 @@
+<svelte:head>
+  <title
+    >What is Spaced Repetition? And how it Improves Pattern
+    Recognition</title
+  >
+  <meta
+    name="description"
+    content="This article explains what spaced repetition is and how to use it to improve your chess game."
+  />
+</svelte:head>
+
 <div class="page-content">
   <section>
     <article>
-      <h2>What is Spaced Repetition?</h2>
+      <h2>
+        What is Spaced Repetition? And how it Improves Pattern
+        Recognition
+      </h2>
       <h5>
-        Learn how to become a better player by utilising spaced
+        <!-- Learn how to become a better player by utilising spaced
         repetition to help you memorise and recognise patterns more
-        efficiently.
+        efficiently. -->
+        Picture yourself in a chess match, facing a challenging position.
+        Suddenly, in a flash of recognition, you realize you've encountered
+        a similar puzzle in the past. That's the power of pattern recognition,
+        and this article will explore how you can recognise more patterns
+        faster with the help of spaced repetition.
       </h5>
       <img src="/images/SRgraph.png" alt="spaced repetition graph" />
 
-      <h3>Lorem ipsum dolor sit amet consec</h3>
+      <h3>What is Spaced repetition?</h3>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et
-        massa mi. Aliquam in hendrerit urna. Pellentesque sit amet
-        sapien fringilla, mattis ligula consectetur, ultrices mauris.
-        Maecenas vitae mattis tellus. Nullam quis imperdiet augue.
-        Vestibulum auctor ornare leo, non suscipit magna interdum eu.
-        Curabitur pellentesque nibh nibh, at maximus ante fermentum
-        sit amet. Pellentesque commodo lacus at sodales sodales.
-        Quisque sagittis orci ut diam condimentum, vel euismod erat
-        placerat. In iaculis arcu eros, eget tempus orci facilisis id.
-        Praesent lorem orci, mattis non efficitur id, ultricies vel
-        nibh. Sed volutpat lacus vitae gravida viverra. Fusce vel
-        tempor elit. Proin tempus, magna id scelerisque vestibulum,
-        nulla ex pharetra sapien, tempor posuere massa neque nec
-        felis. Aliquam sem ipsum, vehicula ac tortor vel, egestas
-        ullamcorper dui. Curabitur at risus sodales, tristique est id,
-        euismod justo. Mauris nec leo non libero sodales lobortis.
-        Quisque a neque pretium, dictum tellus vitae, euismod neque.
-        Nulla facilisi. Phasellus ultricies dignissim nibh ut cursus.
-        Nam et quam sit amet turpis finibus maximus tempor eget augue.
-        Aenean at ultricies lorem. Sed egestas ligula tortor, sit amet
-        mattis ex feugiat non. Duis purus diam, dictum et ante at,
-        commodo iaculis urna. Aenean lacinia, nisl id vehicula
-        condimentum, enim massa.
+        Spaced Repetition is just what the name suggests: reviewing
+        material at intervals, where each interval is longer than the
+        last. This straightforward method has a robust scientific
+        backing for its effectiveness, with a <a
+          href="https://www.edapp.com/blog/evidence-around-spaced-repetition/"
+          target="_blank"
+          >wealth of evidence spanning over 30 years.</a
+        >
       </p>
-      <h3>Ullam aliquam</h3>
+      <h3>Why is spaced repetition effective?</h3>
       <p>
-        Consectetur adipisicing elit. Provident accusamus ipsam
-        temporibus quibusdam, ipsa dolorem dolorum odio eligendi optio
-        inventore perferendis porro officia, at natus, itaque quo et
-        hic repellendus nesciunt eos tenetur quasi ex quia nam!
-        Blanditiis, culpa? Beatae voluptatibus quidem quasi et
-        deserunt enim ad similique iusto repellendus placeat incidunt
-        quo, id, culpa odio, a qui pariatur veniam consequuntur
-        cupiditate fugit quae non? Aspernatur corrupti eveniet nihil
-        non quibusdam, sed harum quasi officiis ullam perferendis
-        voluptatum reprehenderit esse cupiditate soluta deleniti nisi
-        ipsum, illum architecto labore ad sint. Sunt quidem a harum
-        magni cupiditate veniam velit itaque ipsa maxime consequuntur
-        iure autem numquam obcaecati qui, dolorem vero eveniet
-        expedita! Nostrum deserunt facere consectetur recusandae
-        architecto sit fugiat nisi placeat assumenda debitis
-        necessitatibus, suscipit omnis dolorum quibusdam fuga quis
-        odio cumque repellendus dignissimos. Ipsa pariatur placeat
-        adipisci, doloremque autem, ex voluptates voluptatum
-        consequatur corrupti sit, minus ullam maxime labore molestias
-        delectus debitis omnis dolore alias vero rem enim dolorem quis
-        dicta voluptate. Inventore quibusdam, molestiae illum neque
-        aspernatur odit id dignissimos sunt distinctio voluptatibus
-        similique officia quae non architecto eveniet voluptatum sint!
-        Tempora, minima officia? Vero illum tenetur pariatur sunt
-        perferendis doloribus quisquam temporibus distinctio error
-        fugit, quas culpa?
+        The optimal time to review a piece of information is when we
+        are just about to forget it, and each time we review
+        something, it takes longer to forget it. The spaced repetition
+        algorithm is effective because it takes into account both of
+        those factors. After each review, it tries to predict when to
+        show it to you for the best results. This way you can minimise
+        the amount of time spent on studying
       </p>
+      <h3>The power of pattern recognition</h3>
+      <p>
+        In chess, each move can determine the outcome of the game. At
+        its core, chess is about recognizing and capitalizing on
+        patterns. Imagine having the uncanny ability to see the best
+        move in an instant, as if your mind were a finely tuned chess
+        engine. This is the power of pattern recognition.
+      </p>
+      <p>
+        So, how can we significantly enhance our pattern recognition?
+        The answer lies in puzzles-chess puzzles provide an endless
+        stream of pattern after pattern, each capable of turning the
+        outcome of a game around. But finding the solution once after
+        several minutes of playing possible scenarios your head
+        doesn't guarantee that you will spot it again when it comes up
+        in a game. So, how can we increase that chances of finding it
+        in game drastically? That is the problem that ResPawn tries to
+        solve, let's delve deeper into how ResPawn can elevate your
+        chess skills.
+      </p>
+      <h3>Spaced Repetition for Chess</h3>
+      <p>
+        After each puzzle is played, you have the ability to rate the
+        puzzle based on how challenging it was. Based on that rating,
+        ResPawn uses a spaced repetition algorithm to determine when
+        the best time to review the puzzle.
+      </p>
+      <p>
+        Through systematic pattern repetition, you'll increase your
+        likelihood of recognising it in a real game, giving you a
+        higher chance of outplaying your opponent.
+      </p>
+      <h3>Conclusion</h3>
+      <p>
+        Spaced repetition is a scientifically proven and effective
+        technique for memorisation. ResPawn leverages a spaced
+        repetition algorithm to enhance your pattern recognition
+        skills and dominate in games.
+      </p>
+      <p>
+        Go and give it a try! Explore the playlists <a
+          href="/playlists">here</a
+        >
+      </p>
+      <p>Good luck and have fun!</p>
     </article>
   </section>
 </div>
@@ -93,5 +123,11 @@
   }
   p {
     font-size: 1.25rem;
+  }
+  a {
+    color: var(--accent);
+  }
+  a:hover {
+    text-decoration: underline;
   }
 </style>
