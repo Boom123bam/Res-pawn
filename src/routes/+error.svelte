d82c02c8414a51b27874f6ef67487a0345dca86e
Brandon <brandontang0325@gmail.com>
2023-10-09 11:49:08 +0100

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<div class="page-content">
+  <section class="error">
+    <h3>{$page.status} {$page.error.message}</h3>
+    <a href="/">
+      <button class="cta">back to home</button>
+    </a>
+  </section>
+</div>
+
+<style>
+  section.error {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+  }
+</style>
