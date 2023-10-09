47a8e6dbbadf5caf6e9d8f6f6ac2191ec8a72841
Brandon <brandontang0325@gmail.com>
2023-10-09 15:02:38 +0100

    remove .page-content wrapper

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,14 +2,12 @@
   import { page } from "$app/stores";
 </script>
 
-<div class="page-content">
-  <section class="error">
-    <h3>{$page.status} {$page.error.message}</h3>
-    <a href="/">
-      <button class="cta">back to home</button>
-    </a>
-  </section>
-</div>
+<section class="error">
+  <h3>{$page.status} {$page.error.message}</h3>
+  <a href="/">
+    <button class="cta">back to home</button>
+  </a>
+</section>
 
 <style>
   section.error {
