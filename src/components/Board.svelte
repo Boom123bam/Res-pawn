e037bf61ab7504abbe5a90fad733a3f91c1f9b07
Brandon <brandontang0325@gmail.com>
2023-10-19 10:37:15 +0100

    add history

--- src/components/Board.svelte
+++ src/components/Board.svelte
@@ -1,39 +1,67 @@
 <script>
-  import { Chess } from "chess.js";
-  import Square from "./Square.svelte";
-  import { sequenceData } from "../stores/boardStore";
   import "./Board.css";
+  import Square from "./Square.svelte";
   import Promotion from "./Promotion.svelte";
-  import { createEventDispatcher } from "svelte";
   import BoardControls from "./BoardControls.svelte";
   import Svg from "./Svg.svelte";
-  import { browser } from "$app/environment";
+  import { sequenceData } from "../stores/boardStore";
   import { getSetting } from "../modules/localStorage";
+  import { createEventDispatcher } from "svelte";
+  import { browser } from "$app/environment";
+  import { Chess } from "chess.js";
+  import { ChessBoard } from "../modules/chessBoard";
 
   // audio
   let moveBuffer;
   let captureBuffer;
   let context;
+  loadAudioBuffers();
 
   const dispatch = createEventDispatcher();
   let currentSequence = null;
 
-  let board = {
-    chess: new Chess(),
-    displayer: new Chess(),
-    board: null,
+  let board = new ChessBoard();
+
+  const display = {
     highlightedSquares: [],
-    selectedSquare: null,
-    movesBack: 0,
     disabled: false,
-    flipped: false,
-    moveToPromote: "",
-    lastMove: "e4d5",
+    hint: false,
+    solution: false,
+    moveToPromote: null,
     movePlaying: null,
-    showIndicatorOnHover: getSetting("showIndicatorOnHover"),
-    soundOn: getSetting("sound"),
+    board: board.chess.board(),
+    flipped: false,
+    failed: false,
   };
-  board.board = board.chess.board();
+
+  function resetHighlights(resetHintSol = false) {
+    // resets highlights and hints
+
+    display.highlightedSquares = [];
+    // board.selectedSquare = null;
+  }
+
+  function resetHints() {
+    display.hint = false;
+    display.solution = false;
+  }
+
+  // let board = {
+  //   chess: new Chess(),
+  //   displayer: new Chess(),
+  //   board: null,
+  //   highlightedSquares: [],
+  //   selectedSquare: null,
+  //   movesBack: 0,
+  //   disabled: false,
+  //   flipped: false,
+  //   moveToPromote: "",
+  //   lastMove: "e4d5",
+  //   movePlaying: null,
+  //   showIndicatorOnHover: getSetting("showIndicatorOnHover"),
+  //   soundOn: getSetting("sound"),
+  // };
+  // board.board = board.chess.board();
 
   sequenceData.subscribe((newSeqData) => {
     if (newSeqData && !newSeqData?.finished) {
@@ -41,45 +69,46 @@
     }
   });
 
-  // Audio
-  if (browser) {
-    let AudioContext =
-      window.AudioContext || window.webkitAudioContext;
-    context = new AudioContext(); // Make it crossbrowser
-    window
-      .fetch("/sfx/capture.mp3")
-      .then((response) => response.arrayBuffer())
-      .then((arrayBuffer) =>
-        context.decodeAudioData(
-          arrayBuffer,
-          (audioBuffer) => {
-            captureBuffer = audioBuffer;
-          },
-          (error) => console.error(error)
-        )
-      );
-    window
-      .fetch("/sfx/move.mp3")
-      .then((response) => response.arrayBuffer())
-      .then((arrayBuffer) =>
-        context.decodeAudioData(
-          arrayBuffer,
-          (audioBuffer) => {
-            moveBuffer = audioBuffer;
-          },
-          (error) => console.error(error)
-        )
-      );
-  }
-
-  function playAudio(audioBuffer) {
+  function loadAudioBuffers() {
     if (browser) {
-      let source = context.createBufferSource();
-      source.buffer = audioBuffer;
-      source.connect(context.destination);
-      source.start();
+      let AudioContext =
+        window.AudioContext || window.webkitAudioContext;
+      context = new AudioContext(); // Make it crossbrowser
+      window
+        .fetch("/sfx/capture.mp3")
+        .then((response) => response.arrayBuffer())
+        .then((arrayBuffer) =>
+          context.decodeAudioData(
+            arrayBuffer,
+            (audioBuffer) => {
+              captureBuffer = audioBuffer;
+            },
+            (error) => console.error(error)
+          )
+        );
+      window
+        .fetch("/sfx/move.mp3")
+        .then((response) => response.arrayBuffer())
+        .then((arrayBuffer) =>
+          context.decodeAudioData(
+            arrayBuffer,
+            (audioBuffer) => {
+              moveBuffer = audioBuffer;
+            },
+            (error) => console.error(error)
+          )
+        );
     }
   }
+
+  // function playAudio(audioBuffer) {
+  //   if (browser) {
+  //     let source = context.createBufferSource();
+  //     source.buffer = audioBuffer;
+  //     source.connect(context.destination);
+  //     source.start();
+  //   }
+  // }
 
   async function handleSeqLoad(seqData) {
     currentSequence = {
@@ -97,64 +126,70 @@
         timesFailed: 0,
       },
     };
-    board.flipped = currentSequence.start.split(" ")[1] === "w"; // flip board if black is first
-    resetSequence();
+    display.flipped = currentSequence.start.split(" ")[1] === "w"; // flip board if black is first
+    // resetSequence(); TODO
+    board.load(currentSequence.start);
     updateBoard();
     await timeout(100);
     await movePiece(currentSequence.moves[0]);
     updateBoard();
   }
 
-  function resetSequence() {
-    // reset seq and board
-    board.chess.load(currentSequence.start);
-    resetBoardHighlights(false, true);
-    board.movesBack = 0;
-    currentSequence.step = 1;
-    currentSequence.failed = false;
-    currentSequence.finished = false;
-    currentSequence.deductedPoints = 0;
-  }
+  // function resetSequence() {
+  //   // reset seq and board
+  //   board.chess.load(currentSequence.start);
+  //   resetBoardHighlights(false, true);
+  //   board.movesBack = 0;
+  //   currentSequence.step = 1;
+  //   currentSequence.failed = false;
+  //   currentSequence.finished = false;
+  //   currentSequence.deductedPoints = 0;
+  // }
 
-  function resetBoardHighlights(
-    disable = null,
-    resetHintSol = false
-  ) {
-    // resets highlights and hints
-    board.highlightedSquares = [];
-    board.selectedSquare = null;
-    if (disable != null) {
-      board.disabled = disable;
-    }
-    if (resetHintSol) {
-      currentSequence.hint = false;
-      currentSequence.solution = false;
-    }
-  }
+  // function resetBoardHighlights(
+  //   disable = null,
+  //   resetHintSol = false
+  // ) {
+  //   // resets highlights and hints
+  //   board.highlightedSquares = [];
+  //   board.selectedSquare = null;
+  //   if (disable != null) {
+  //     display.disabled = disable;
+  //   }
+  //   if (resetHintSol) {
+  //     currentSequence.hint = false;
+  //     currentSequence.solution = false;
+  //   }
+  // }
 
   function updateBoard() {
-    if (board.movesBack == -1) {
-      // user got the sequence wrong, show the last move in board.displayer
-      resetBoardHighlights(true, true);
-      board.displayer.load(currentSequence.failed);
-      board.board = board.displayer.board();
-    } else if (board.movesBack) {
-      //switch to history mode
-      resetBoardHighlights(true, true);
-      loadMovesBack(board.movesBack);
-      board.board = board.displayer.board();
-    } else {
-      if (
-        board.disabled &&
-        (!currentSequence ||
-          (!currentSequence.failed && !currentSequence.finished))
-      ) {
-        //switch to normal mode
-        board.disabled = false;
-      }
-      board.board = board.chess.board();
-    }
+    resetHighlights();
+    display.board = board.chess.board();
   }
+
+  // function updateBoard() {
+  //   if (board.movesBack == -1) {
+  //     // user got the sequence wrong, show the last move in board.displayer
+  //     resetBoardHighlights(true, true);
+  //     board.displayer.load(currentSequence.failed);
+  //     board.board = board.displayer.board();
+  //   } else if (board.movesBack) {
+  //     //switch to history mode
+  //     resetBoardHighlights(true, true);
+  //     loadMovesBack(board.movesBack);
+  //     board.board = board.displayer.board();
+  //   } else {
+  //     if (
+  //       display.disabled &&
+  //       (!currentSequence ||
+  //         (!currentSequence.failed && !currentSequence.finished))
+  //     ) {
+  //       //switch to normal mode
+  //       display.disabled = false;
+  //     }
+  //     board.board = board.chess.board();
+  //   }
+  // }
 
   function getSquare(row, col) {
     // convert row and col to square notation
@@ -163,30 +198,26 @@
     }`;
   }
 
-  function checkIfPromotion(move) {
-    // check if the move is promotion
-    // if moveTo is last row and if piece is pawn
+  // function checkIfPromotion(move) {
+  //   // check if the move is promotion
+  //   // if moveTo is last row and if piece is pawn
 
-    const piece = board.chess.get(move.substring(0, 2));
-    if (
-      piece.type == "p" &&
-      ((move[3] == "1" && piece.color == "b") ||
-        (move[3] == "8" && piece.color == "w"))
-    ) {
-      return true;
-    }
-    return false;
-  }
+  //   const piece = board.chess.get(move.substring(0, 2));
+  //   if (
+  //     piece.type == "p" &&
+  //     ((move[3] == "1" && piece.color == "b") ||
+  //       (move[3] == "8" && piece.color == "w"))
+  //   ) {
+  //     return true;
+  //   }
+  //   return false;
+  // }
 
   function timeout(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
-  async function movePiece(
-    move,
-    useDisplayer = false,
-    duration = 0.1
-  ) {
+  async function movePiece(move, duration = 0.1) {
     if (board.soundOn) {
       if (board.chess.get(move.substring(2, 4))) {
         playAudio(captureBuffer);
@@ -194,13 +225,12 @@
     }
 
     // play animation
-    board.movePlaying = move;
+    display.movePlaying = move;
 
-    if (!useDisplayer) board.chess.move(move);
-    else board.displayer.move(move);
-    if (board.movesBack == 0) board.lastMove = move;
+    board.makeMove(move);
+    // if (board.movesBack == 0) board.lastMove = move; TODO history
     await timeout(duration * 1000);
-    board.movePlaying = null;
+    display.movePlaying = null;
   }
 
   async function updateSequence(move = null) {
@@ -232,7 +262,7 @@
     if (currentSequence.step === currentSequence.moves.length) {
       // finished
       currentSequence.finished = true;
-      board.disabled = true;
+      display.disabled = true;
       // dispatch event and return stats
       dispatch("finish", currentSequence.stats);
     }
@@ -240,33 +270,33 @@
     return true;
   }
 
-  function loadMovesBack(movesBack) {
-    const history = board.chess.history({ verbose: true });
-    board.displayer.load(history[history.length - movesBack].before);
-  }
+  // function loadMovesBack(movesBack) {
+  //   const history = board.chess.history({ verbose: true });
+  //   board.displayer.load(history[history.length - movesBack].before);
+  // }
 
-  function getMoves(square) {
-    // get valid moves for a square
-    return board.chess
-      .moves({
-        square,
-        verbose: true,
-      })
-      .map((move) => {
-        return move.to;
-      });
-  }
+  // function getMoves(square) {
+  //   // get valid moves for a square
+  //   return board.chess
+  //     .moves({
+  //       square,
+  //       verbose: true,
+  //     })
+  //     .map((move) => {
+  //       return move.to;
+  //     });
+  // }
 
-  async function handleMoveClick(move, promotion = "") {
-    // if the move is a promotion
-    if (!promotion & checkIfPromotion(move)) {
-      board.moveToPromote = move;
+  async function handleMoveClick(move, promoteToPiece = "") {
+    if (!promoteToPiece & board.checkIfPromotion(move)) {
+      // if the move is a promotion
+      display.moveToPromote = move;
       return;
     }
 
-    move = move + promotion;
+    move += promoteToPiece;
     if (currentSequence) {
-      resetBoardHighlights(null, true);
+      resetHints();
       if (await updateSequence(move)) {
         // correct move, move piece
         await movePiece(move);
@@ -277,23 +307,20 @@
         }
       } else {
         // wrong move
-        board.displayer.load(board.chess.fen());
-        resetBoardHighlights(true);
-        await movePiece(move, true);
+        await movePiece(move);
         // check if checkmate (somtimes there are 2 answers for the last move)
-        if (board.displayer.isCheckmate()) {
+        if (board.chess.isCheckmate()) {
           currentSequence.finished = true;
-          board.disabled = true;
           // dispatch event and return stats
           dispatch("finish", currentSequence.stats);
-          movePiece(move);
         } else {
+          display.disabled = true;
           board.movesBack = -1;
-          currentSequence.failed = board.displayer.fen();
+          currentSequence.failed = board.chess.fen();
           currentSequence.stats.timesFailed++;
         }
+        updateBoard();
       }
-      updateBoard();
     } else {
       // no sequence, move piece
       await movePiece(move);
@@ -302,84 +329,88 @@
   }
 
   function handlePieceClick(id) {
-    if (board.disabled) return;
-    if (board.highlightedSquares.length == 0) {
+    if (display.disabled) return;
+    if (display.highlightedSquares.length == 0) {
       // select the piece and highlight possible moves
-      board.highlightedSquares = getMoves(id);
-      if (board.highlightedSquares.length) board.selectedSquare = id;
-    } else if (board.highlightedSquares.includes(id)) {
+      display.highlightedSquares = board.getPossibleMoves(id);
+      if (display.highlightedSquares.length)
+        board.selectedSquare = id;
+    } else if (display.highlightedSquares.includes(id)) {
       handleMoveClick(board.selectedSquare + id);
       // check if there is a sequence
-      resetBoardHighlights();
     } else {
       // un-select and un-highlight
-      resetBoardHighlights();
+      resetHighlights();
     }
   }
 
-  function handlePromotion(e) {
-    handleMoveClick(board.moveToPromote, e.detail);
-    board.moveToPromote = "";
-  }
+  // function handlePromotion(e) {
+  //   handleMoveClick(board.moveToPromote, e.detail);
+  //   board.moveToPromote = "";
+  // }
 
   function handleBackButton() {
-    if (board.movesBack < board.chess.history().length) {
-      board.movesBack++;
-      updateBoard();
-    }
+    console.log(board.showPrevMove());
+    updateBoard();
+    //   if (board.movesBack < board.chess.history().length) {
+    //     board.movesBack++;
+    //     updateBoard();
+    //   }
   }
 
   function handleNextButton() {
-    if (
-      board.movesBack > 0 ||
-      (board.movesBack == 0 && currentSequence?.failed)
-    ) {
-      board.movesBack--;
-      updateBoard();
-    }
-  }
-
-  function handleFlipButton() {
-    board.flipped = !board.flipped;
-  }
-
-  function handleRetryLastMoveButton() {
-    board.disabled = false;
-    currentSequence.failed = false;
-    board.movesBack = 0;
+    console.log(board.showNextMove());
     updateBoard();
+    //   if (
+    //     board.movesBack > 0 ||
+    //     (board.movesBack == 0 && currentSequence?.failed)
+    //   ) {
+    //     board.movesBack--;
+    //     updateBoard();
+    //   }
   }
+
+  // function handleFlipButton() {
+  //   board.flipped = !board.flipped;
+  // }
+
+  // function handleRetryLastMoveButton() {
+  //   display.disabled = false;
+  //   currentSequence.failed = false;
+  //   board.movesBack = 0;
+  //   updateBoard();
+  // }
 
   function handleHintButton() {
     currentSequence.hint = true;
     currentSequence.stats.hintsUsed++;
   }
 
-  function handleSolutionButton() {
-    currentSequence.solution = true;
-    currentSequence.stats.solsUsed++;
-  }
+  // function handleSolutionButton() {
+  //   currentSequence.solution = true;
+  //   currentSequence.stats.solsUsed++;
+  // }
 </script>
 
 <div class="board-component-wrapper">
   <div class="board-wrapper">
-    {#if board.moveToPromote}
+    <!-- {#if board.moveToPromote}
       <Promotion
         on:promotion={handlePromotion}
         color={board.chess.get(board.moveToPromote.substring(0, 2))
           .color}
       />
-    {/if}
+    {/if} -->
     <div class="board-padding">
       <div class={`board${board.flipped ? " flipped" : " normal"}`}>
-        {#each board.board as row, rowNum}
+        {#each display.board as row, rowNum}
           {#each row as square, colNum}
             <Square
               id={getSquare(rowNum, colNum)}
               squareColor={board.chess.squareColor(
                 getSquare(rowNum, colNum)
               )}
-              highlighted={board.highlightedSquares.includes(
+              highlighted={display.highlightedSquares.includes(
                 getSquare(rowNum, colNum)
               )}
               {square}
@@ -393,18 +424,18 @@
                   currentSequence?.step
                 ].substring(2, 4) == getSquare(rowNum, colNum)}
               lastMove={board.movesBack == 0 &&
-                (board.lastMove.substring(0, 2) ==
+                (board.lastMove?.substring(0, 2) ==
                   getSquare(rowNum, colNum) ||
-                  board.lastMove.substring(2, 4) ==
+                  board.lastMove?.substring(2, 4) ==
                     getSquare(rowNum, colNum))}
               order={board.flipped
                 ? 63 - (rowNum * 8 + colNum)
                 : rowNum * 8 + colNum}
               flipped={board.flipped}
-              moveTo={board.movePlaying
-                ? board.movePlaying.substring(0, 2) ==
+              moveTo={display.movePlaying
+                ? display.movePlaying.substring(0, 2) ==
                   getSquare(rowNum, colNum)
-                  ? board.movePlaying.substring(2, 4)
+                  ? display.movePlaying.substring(2, 4)
                   : null
                 : null}
               showIndicatorOnHover={board.showIndicatorOnHover}
@@ -436,7 +467,7 @@
                 }
             target="_blank"
           > -->
-          <a
+          <!-- <a
             href={board.movesBack === -1
               ? `https://lichess.org/analysis/standard/${
                   currentSequence.failed
@@ -461,7 +492,7 @@
             <button class="analyze" title="analyze in lichess"
               ><Svg name="search" color="var(--secondary)" /></button
             >
-          </a>
+          </a> -->
         </div>
         <BoardControls
           showRetryLastMove={currentSequence?.failed}
@@ -476,10 +507,7 @@
           flashingNext={board.movesBack > 0}
           on:back={handleBackButton}
           on:next={handleNextButton}
-          on:flip={handleFlipButton}
-          on:retryLastMove={handleRetryLastMoveButton}
           on:hint={handleHintButton}
-          on:solution={handleSolutionButton}
         />
         <div class="after">
           <slot name="after" />
---
+++ src/modules/chessBoard.ts
@@ -0,0 +1,63 @@
+import { Chess, Square } from "chess.js";
+
+export class ChessBoard {
+  history: string[] = [];
+  movesBack = 0;
+  chess: Chess = new Chess();
+
+  constructor(fen = null) {}
+
+  load(fen) {
+    this.history = [];
+    this.movesBack = 0;
+    this.chess.load(fen);
+  }
+
+  makeMove(move: string) {
+    if (this.movesBack == 0) this.history.push(move);
+    this.chess.move(move);
+  }
+
+  getPossibleMoves(square: Square) {
+    return this.chess
+      .moves({
+        square,
+        verbose: true,
+      })
+      .map((move) => {
+        return move.to;
+      });
+  }
+
+  checkIfPromotion(move) {
+    // check if the move is promotion
+    // if moveTo is last row and if piece is pawn
+    const piece = this.chess.get(move.substring(0, 2));
+    if (
+      piece.type == "p" &&
+      ((move[3] == "1" && piece.color == "b") ||
+        (move[3] == "8" && piece.color == "w"))
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  showPrevMove() {
+    if (this.history.length > this.movesBack) {
+      this.movesBack++;
+      this.chess.undo();
+      return this.history[this.history.length - this.movesBack];
+    }
+    return null;
+  }
+  showNextMove() {
+    if (this.movesBack > 0) {
+      const move = this.history[this.history.length - this.movesBack];
+      this.makeMove(move);
+      this.movesBack--;
+      return move;
+    }
+    return null;
+  }
+}
