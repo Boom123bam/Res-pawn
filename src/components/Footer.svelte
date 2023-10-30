f55f3ab1e1e78becde318460953387f5b1979e54
Brandon <brandontang0325@gmail.com>
2023-10-30 08:43:05 +0000

    change footer style

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -9,26 +9,26 @@
       href="https://github.com/Boom123bam/ResPawn"
       target="_blank"
     >
-      <Svg name="github" color="var(--text)" size="2.5rem" />
+      <Svg name="github" color="var(--text)" size="1.75rem" />
     </a>
     <a
       class="float"
       href="https://discord.gg/b83Mkaghtb"
       target="_blank"
     >
-      <Svg name="discord" color="var(--text)" size="2.5rem" />
+      <Svg name="discord" color="var(--text)" size="1.75rem" />
     </a>
   </div>
   <div class="links">
-    <h3>Links</h3>
+    <h4>Links</h4>
     <a class="link" href="/">
-      <h5>home</h5>
+      <h6>home</h6>
     </a>
     <a class="link" href="/playlists">
-      <h5>browse</h5>
+      <h6>browse</h6>
     </a>
     <a class="link" href="/article/spaced-repetition">
-      <h5>about spaced repetition</h5>
+      <h6>about spaced repetition</h6>
     </a>
   </div>
   <div class="contact">
@@ -46,8 +46,8 @@
     align-items: center;
   }
   section.footer {
-    gap: 2.75rem;
-    padding: 5rem 0;
+    gap: 1.5rem;
+    padding: 2.5rem 0 3rem;
     border-top: var(--border);
   }
   .logos {
@@ -55,7 +55,13 @@
     gap: 1.75rem;
   }
   .links {
-    gap: 1rem;
+    gap: 0.75rem;
+  }
+  .links h4 {
+    margin-bottom: 0.25rem;
+  }
+  .contact {
+    font-size: 0.75rem;
   }
   .contact a {
     font-weight: 700;
