38d7746f7f98b71d323abef8741a818fd9c67f63
Brandon <brandontang0325@gmail.com>
2023-10-30 07:40:04 +0000

    fix resend button

--- src/routes/auth/verify/+page.svelte
+++ src/routes/auth/verify/+page.svelte
@@ -9,23 +9,46 @@
   // signing out and in refreshes auth token so that email_verified is updated in firestore rules
   // https://stackoverflow.com/questions/68025606/firebase-web-sdk-refreshing-auth-token-so-that-email-verified-is-updated-in-fir
 
-  let resendButton;
+  let disabled = false;
   let verified = false;
+  let secsCounter = 5;
+  let showSentIndicator = false;
 
-  function sendEmail() {
-    resendButton.disabled = true;
-    setTimeout(function () {
-      resendButton.disabled = false;
-    }, 2000);
-    if (!resendButton.disabled) {
-      sendEmailVerification(auth.currentUser, actionCodeSettings);
+  function sendEmail(user = auth.currentUser) {
+    if (!disabled) {
+      // sendEmailVerification(user, actionCodeSettings);
     }
+    disabled = true;
+    secsCounter = 15;
+    showSentIndicator = true;
+    setTimeout(() => {
+      showSentIndicator = false;
+    }, 2000);
+    startCountDown();
+  }
+
+  function startCountDown() {
+    decrementCount();
+  }
+  function decrementCount() {
+    setTimeout(() => {
+      secsCounter--;
+
+      if (secsCounter <= 0) {
+        endCountDown();
+        return;
+      }
+
+      decrementCount();
+    }, 1000);
+  }
+  function endCountDown() {
+    disabled = false;
   }
 
   auth.onAuthStateChanged(function (user) {
     if (user) {
-      if (!user.emailVerified)
-        sendEmailVerification(user, actionCodeSettings);
+      if (!user.emailVerified) sendEmail(user);
       else verified = true;
     }
   });
@@ -35,11 +58,48 @@
   {#if !verified}
     <h3>Verification sent</h3>
     <h6>Check your email</h6>
-    <button class="cta" bind:this={resendButton} on:click={sendEmail}>
+    <button
+      class={`cta resend${showSentIndicator ? " sent" : ""}`}
+      on:click={() => sendEmail()}
+      {disabled}
+    >
       resend verification
     </button>
+    {#if secsCounter}
+      <p>try again in {secsCounter} seconds</p>
+    {/if}
     <h6>Please sign in again after verification</h6>
   {:else}
     <h3>already verified</h3>
   {/if}
 </div>
+
+<style>
+  button.resend.sent:disabled {
+    opacity: 1;
+  }
+  button.resend {
+    position: relative;
+    overflow: hidden;
+    transition: 0.5s;
+  }
+
+  button.resend::after {
+    position: absolute;
+    top: 0;
+    left: 0;
+    content: "sent";
+    width: 100%;
+    height: 100%;
+    background: rgb(85, 141, 0);
+    color: var(--secondary);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: 0.5s;
+    transform: translateY(-100%);
+  }
+  button.resend.sent::after {
+    transform: none;
+  }
+</style>
