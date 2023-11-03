98ec6dcd34ffcd8cfd54086ee16be68e4d6d9bd1
Brandon <brandontang0325@gmail.com>
2023-11-03 07:58:04 +0000

    add space rep tests

--- __tests__/spacedRep.test.js
+++ __tests__/spacedRep.test.js
@@ -1,7 +1,12 @@
-const getEasinessChange =
-  require("../src/modules/spacedRep").getEasinessChange;
+const { Timestamp } = require("firebase/firestore");
+const {
+  getEasinessChange,
+  getTimeToNextReview,
+  updateSeqData,
+  getNextSeq,
+} = require("../src/modules/spacedRep");
 
-describe("getEasinessChange", () => {
+describe(getEasinessChange, () => {
   it("impacts easiness negatively when grade is lowest", () => {
     const change = getEasinessChange(0);
     expect(change).toBeLessThan(0);
@@ -9,5 +14,84 @@ describe("getEasinessChange", () => {
   it("impacts easiness positively when grade is highest", () => {
     const change = getEasinessChange(2);
     expect(change).toBeGreaterThan(0);
+  });
+});
+
+describe(getTimeToNextReview, () => {
+  it("returns a larger number (interval in mins) if easiness is more", () => {
+    expect(getTimeToNextReview(1.5)).toBeGreaterThan(
+      getTimeToNextReview(0.5)
+    );
+  });
+});
+
+describe(updateSeqData, () => {
+  const seqData = {
+    timesStudied: 1,
+    easiness: 2.125,
+    nextReview: Timestamp.now(),
+  };
+
+  it("will increase easiness if grade is high", () => {
+    const newSeqData = updateSeqData(2, { ...seqData });
+    expect(newSeqData.easiness).toBeGreaterThan(seqData.easiness);
+  });
+
+  it("will set nextReview as a future timestamp", () => {
+    const newSeqData = updateSeqData(2, { ...seqData });
+    expect(newSeqData.nextReview.seconds).toBeGreaterThan(
+      seqData.nextReview.seconds
+    );
+  });
+
+  it("will decrease easiness if grade is low", () => {
+    const newSeqData = updateSeqData(0, { ...seqData });
+    expect(newSeqData.easiness).toBeLessThan(seqData.easiness);
+  });
+});
+
+describe(getNextSeq, () => {
+  const playedSeqsData = {
+    Nbx8S: {
+      timesStudied: 1,
+      easiness: 2.75,
+      nextReview: {
+        seconds: 1698529953.7909,
+        nanoseconds: 462000000,
+      },
+    },
+    wgJQk: {
+      timesStudied: 1,
+      easiness: 2.125,
+      nextReview: {
+        seconds: 1697906452.2437947,
+        nanoseconds: 128000000,
+      },
+    },
+  };
+
+  const unplayedSeqsIDs = [
+    "04NEC",
+    "04Q6b",
+    "0GcoK",
+    "0HEko",
+    "0Hd99",
+    "0LG18",
+    "0LZqn",
+    "0MP31",
+  ];
+
+  it("will return a random seq ID if math.random < chanceOfRandomSeq", () => {
+    Math.random = () => 0;
+    expect(unplayedSeqsIDs).toContain(
+      getNextSeq(playedSeqsData, unplayedSeqsIDs)
+    );
+  });
+
+  it("will return a played seq ID if math.random > chanceOfRandomSeq", () => {
+    Math.random = () => 1;
+    expect(Object.keys(playedSeqsData)).toContain(
+      getNextSeq(playedSeqsData, unplayedSeqsIDs)
+    );
   });
 });
