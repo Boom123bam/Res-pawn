2ef36ed1c182eb0d573b6a93770298f66543a7f4
Brandon <45514686+Boom123bam@users.noreply.github.com>
2024-04-15 11:30:10 +0100

    Formatting

--- src/components/BoardInterface.svelte
+++ src/components/BoardInterface.svelte
@@ -1,9 +1,9 @@
 <script>
-    import { createEventDispatcher } from "svelte";
-    import Board from "./Board.svelte";
-    import SeqInfo from "./SeqInfo.svelte";
-    import BoardControl from "./BoardControl.svelte";
-    import Svg from "./Svg.svelte";
+    import { createEventDispatcher } from 'svelte';
+    import Board from './Board.svelte';
+    import SeqInfo from './SeqInfo.svelte';
+    import BoardControl from './BoardControl.svelte';
+    import Svg from './Svg.svelte';
 
     export let seqInfo, enableNextButton;
 
@@ -12,35 +12,35 @@
 
 <div class="layout">
     <div class="board-container">
-        <Board on:finish={(e) => dispatch("finish", e.detail)} />
+        <Board on:finish={(e) => dispatch('finish', e.detail)} />
     </div>
 
     <div class="side">
         {#if seqInfo}
             <div class="seq-info-wrapper">
                 <SeqInfo
-                    on:morePressed={() => dispatch("morePressed")}
+                    on:morePressed={() => dispatch('morePressed')}
                     {seqInfo}
                 />
             </div>
         {/if}
         <div class="flip button-wrapper">
-            <BoardControl buttonType={"flip"} />
+            <BoardControl buttonType={'flip'} />
         </div>
         <div class="hint button-wrapper">
-            <BoardControl buttonType={"hint + solution + retry"} />
+            <BoardControl buttonType={'hint + solution + retry'} />
         </div>
         <div class="arrows button-wrapper">
-            <BoardControl buttonType={"arrows"} />
+            <BoardControl buttonType={'arrows'} />
         </div>
         <div class="analyse button-wrapper">
-            <BoardControl buttonType={"analyse"} />
+            <BoardControl buttonType={'analyse'} />
         </div>
 
         <button
             class="next primary shadow"
             disabled={!enableNextButton}
-            on:click={() => dispatch("next")}
+            on:click={() => dispatch('next')}
         >
             <h4>next</h4>
             <Svg name="expand_right_double" />
@@ -105,10 +105,10 @@
             );
             display: grid;
             grid-template:
-                "info   info    flip"
-                "info   info    analyse"
-                "board  board   board "
-                "hint   arrows  next";
+                'info   info    flip'
+                'info   info    analyse'
+                'board  board   board '
+                'hint   arrows  next';
             column-gap: var(--gap-sm);
             row-gap: var(--gap-md);
             grid-template-columns: 27% 1fr 27%;
@@ -164,9 +164,9 @@
             );
             display: grid;
             grid-template:
-                "info  info   info    flip   analyse"
-                "board board  board   board  board"
-                "hint  hint   arrows  next   next";
+                'info  info   info    flip   analyse'
+                'board board  board   board  board'
+                'hint  hint   arrows  next   next';
             grid-template-columns: 12% 12% 1fr 12% 12%;
         }
     }
